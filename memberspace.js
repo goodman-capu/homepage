2c9234ff14d65e77097a7da4051192fb5cfbcc14
Frank <goodman.capu@gmail.com>
2020-10-14 01:58:17 -0700

    Initial setup for simple test

---
+++ memberspace.js
@@ -0,0 +1,7 @@
+var MemberSpace = window.MemberSpace || {subdomain: "fanzhikang"};
+(function(d){
+  var s = d.createElement("script");
+  s.src = "https://cdn.memberspace.com/scripts/widgets.js";
+  var e = d.getElementsByTagName("script")[0];
+  e.parentNode.insertBefore(s,e);
+}(document));
